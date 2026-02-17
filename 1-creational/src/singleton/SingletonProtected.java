03b85153ec708942119900d88fef5ccdddc274e9
lbourke1234 <lbourke1234@gmail.com>
2026-02-17 15:56:51 +0000

    Completed Singleton Implementation

--- 1-creational/src/singleton/SingletonProtected.java
+++ 1-creational/src/singleton/SingletonProtected.java
@@ -1,8 +1,10 @@
 package singleton;
 
 public class SingletonProtected {
+    private static SingletonProtected instance;
     public static SingletonProtected getInstance() {
-        // TODO: implement Singleton
-        return null;
+        if (instance == null)
+            instance = new SingletonProtected();
+        return instance;
     }
 }

31795b4523afc7903dbca989c0bce4555b0085a2
lbourke1234 <lbourke1234@gmail.com>
2026-02-17 15:41:19 +0000

    Completed Builder Implementation

--- 1-creational/src/builder/SportsCarBuilder.java
+++ 1-creational/src/builder/SportsCarBuilder.java
@@ -2,44 +2,49 @@ package builder;
 
 public class SportsCarBuilder implements CarBuilder{
 
+    private Car car;
+
+    public SportsCarBuilder(){
+        this.car = new Car("SPORTS");
+    }
     @Override
     public void buildBodyStyle() {
-        // TODO: implement
+        car.setBodyStyle("External dimensions: overall length (inches): 192.3, overall width (inches): 75.5, overall height (inches): 54.2, wheelbase (inches): 112.3, front track (inches): 63.7, rear track (inches): 64.1 and curb to curb turning circle (feet): 37.7");
     }
 
     @Override
     public void buildPower(){
-        // TODO: implement
+        car.setPower("323 hp @ 6,800 rpm; 278 ft lb of torque @ 4,800 rpm");
     }
 
     @Override
     public void buildEngine() {
-        // TODO: implement
+        car.setEngine("3.6L V 6 DOHC and variable valve timing");
     }
 
     @Override
     public void buildBreaks() {
-        // TODO: implement
+        car.setBreaks("Four-wheel disc brakes: two ventilated. Electronic brake distribution. StabiliTrak stability control");
     }
 
     @Override
     public void buildSeats() {
-        // TODO: implement
+        car.setSeats("Driver sports front seat with one power adjustments manual height, front passenger seat sports front seat with one power adjustments\n" +
+                " Windows: Front windows with one-touch on two windows");
     }
 
     @Override
     public void buildWindows() {
-        // TODO: implement
+        car.setWindows("Front windows with one-touch on two windows");
     }
 
     @Override
     public void buildFuelType() {
-        // TODO: implement
+        car.setFuelType("Gasoline 17 MPG city, 28 MPG highway, 20 MPG combined and 380 mi. range");
     }
 
     @Override
     public Car getCar(){
-        // TODO: implement
-        return null;
+        return car;
     }
 }
